af47ebb80d63b5707bc36170b363a562656b4925
Javier Anchundia Rosado <jaanrosa@espol.edu.ec>
2020-10-22 17:51:28 -0500

    Reenvio del commit

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -5,15 +5,5 @@ public class CarInsurance {
 	protected int age;
 	protected String sex;
 	protected Boolean marital_status;
-	protected PremiumCalculation premiumCalculation;
-	protected int precioPagar;
-	protected CarInsurance me;
-	
-	private CarInsurance() {}
-	
-	public CarInsurance(int age, String sex, Boolean marital_status) {
-		me = new CarInsurance();
-		premiumCalculation = new PremiumCalculation();
-		precioPagar = premiumCalculation.calcularPremio(me);
-	}
+
 }	
