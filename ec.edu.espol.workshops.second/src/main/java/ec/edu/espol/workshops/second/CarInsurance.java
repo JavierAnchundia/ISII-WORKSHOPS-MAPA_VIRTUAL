ce852920db50ed39c90ea3a3bc3f3d6a895ba7ba
Carla Sánchez <carmsanc@espol.edu.ec>
2020-10-22 17:00:54 -0500

    Creation of Class CarInsurance and its attributes

---
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -0,0 +1,13 @@
+package ec.edu.espol.workshops.second;
+
+public class CarInsurance {
+	
+	protected int age;
+	protected String sex;
+	protected Boolean marital_status;
+	
+	final int BASE_PREMIUN = 500;
+	
+	
+
+}
