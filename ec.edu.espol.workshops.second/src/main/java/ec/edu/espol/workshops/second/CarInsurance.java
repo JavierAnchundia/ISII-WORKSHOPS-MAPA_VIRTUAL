43b39836820a72b3ce238691a05039ef9ed4d348
bryantutiven2 <bryan.tutiven.2@outlook.com>
2020-10-22 17:26:15 -0500

    Getters and Setters

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -7,6 +7,34 @@ public class CarInsurance {
 	protected Boolean marital_status;
 	
 	final int BASE_PREMIUN = 500;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public Boolean getMarital_status() {
+		return marital_status;
+	}
+
+	public void setMarital_status(Boolean marital_status) {
+		this.marital_status = marital_status;
+	}
+
+	public int getBASE_PREMIUN() {
+		return BASE_PREMIUN;
+	}
 	
 	
 
