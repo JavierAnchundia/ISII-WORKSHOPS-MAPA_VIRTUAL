6d8a455e33af14d2cdd6f09595ba67ad3221f49a
bryantutiven2 <bryan.tutiven.2@outlook.com>
2020-10-29 18:17:01 -0500

    Se arreglo ciertos warnings encontrados por Checkstyle

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Main.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Main.java
@@ -1,36 +1,27 @@
 package ec.edu.espol.workshops.second;
-import java.util.*;
+
+import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Ingrese el sexo M -> Masculino, F -> Femenino: ");
 		String sex = sc.nextLine();
-		
 		System.out.println("Ingrese la edad: ");
 		int edad = sc.nextInt();
-		
 		System.out.println("Tiene licencia ? si es si responda true, caso contrario false: ");
 		Boolean licencia = sc.nextBoolean();
-		
 		System.out.println("Ingrese estado del matrimonio true -> para casado, false -> para no casado: ");
 		Boolean marital_status = sc.nextBoolean();
-		
 		CarInsurance car = new CarInsurance();
 		car.setAge(edad);
 		car.setMarital_status(marital_status);
 		car.setSex(sex);
 		car.setLicense(licencia);
-		
 		PremiumCalculation premiumCalc = new PremiumCalculation();
 		Policies policies = new Policies();
-//		boolean estado = policies.validCustomer(car);
-		
 		Integer calculoMonto = premiumCalc.calcularPremio(car);
-
 		System.out.println(policies.validCustomer(car, calculoMonto));
-
+		sc.close();
 	}
-
 }
--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Policies.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Policies.java
@@ -2,15 +2,10 @@ package ec.edu.espol.workshops.second;
 
 public class Policies {
 
-	public Policies(){
-		
-	}
-	public int validCustomer(CarInsurance customer, Integer valor) {
-		
-		if(customer.getAge() >80 || !customer.getLicense() || valor == null) {
-			return -1;
-		}
-		else {
+	public Policies(){}
+	
+	public int validCustomer(CarInsurance customer, Integer valor){
+		if(customer.getAge() >80 || !customer.getLicense() || valor == null){ return -1; } else{
 			return valor;
 		}
 	}
