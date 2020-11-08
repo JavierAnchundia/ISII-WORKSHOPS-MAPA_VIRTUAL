9d9ed5996afc25d8516fe66074510efcd5317acd
bryantutiven2 <bryan.tutiven.2@outlook.com>
2020-11-08 11:42:16 -0500

    Merge con rama main

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Policies.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Policies.java
@@ -1,13 +1,14 @@
 package ec.edu.espol.workshops.second;
 
 public class Policies {
-
+	
+	final static int max_age = 80;
 	public Policies(){
 		
 	}
 	public int validCustomer(CarInsurance customer, Integer valor) {
 		
-		if(customer.getAge() >80 || !customer.getLicense() || valor == null) {
+		if(customer.getAge() > max_age || !customer.getLicense() || valor == null) {
 			return -1;
 		}
 		else {
