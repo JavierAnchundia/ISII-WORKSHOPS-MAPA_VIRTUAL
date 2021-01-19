31e0e021658f3b3fdc11b564f9fc66b89560d238
bryantutiven2 <bryan.tutiven.2@outlook.com>
2021-01-19 11:24:56 -0500

    A new policy associated with the calculation of the premium was added as requested by the client in his request

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/PremiumCalculation.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/PremiumCalculation.java
@@ -5,15 +5,21 @@ public class PremiumCalculation {
 	}
 	public Integer calcularPremio(CarInsurance customer){
 		Integer montoPagar = customer.getbasePremium();
-		if(customer.getSex().equals("M") && !customer.getMarital_status() && customer.getAge()<25) {
-			return montoPagar + 1500;
-		}else if(customer.getSex().equals("F") || customer.getMarital_status()){
-			montoPagar = montoPagar - 200;
-		}else if(customer.getAge() >= 45 && customer.getAge() <= 65){
-			montoPagar = montoPagar - 100;
-		}else{
-			montoPagar = null;
+		if(customer.getSex().equals("M") && !customer.getMarital_status() && customer.getAge()<=25) {
+			montoPagar = montoPagar + 1500;
 		}
+		if(customer.getSex().equals("F") || customer.getMarital_status()){
+			montoPagar = montoPagar - 200;
+		}
+		if(customer.getAge() >= 45 && customer.getAge() <= 65){
+			montoPagar = montoPagar - 100;
+		}
+		if(customer.getAge() > 25 && customer.getAge() < 45 ){
+			montoPagar = montoPagar - 100;
+		}
+//		else{
+//			montoPagar = null;
+//		}
 		return montoPagar;
 	}
 }	
